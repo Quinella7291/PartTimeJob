cff35461d7ca196c33418e26f6c3d165b69aab90
Horvey <543000463@qq.com>
2019-03-17 22:08:54 +0800

    第一次提交

---
+++ src/com/horvey/Controller/MainController.java
@@ -0,0 +1,55 @@
+package com.horvey.Controller;
+
+import com.horvey.Service.MainService;
+import com.horvey.pojo.Job;
+import com.horvey.pojo.Message;
+import com.horvey.pojo.User;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+public class MainController {
+
+    @Resource
+    private MainService service;
+
+    @RequestMapping("/index")
+    public Message index(String type, Integer count) {
+        List<Job> jobs = service.getAll(null, 10);
+        return new Message(0, jobs);
+    }
+
+    @RequestMapping("/detail/{id}")
+    public Message detail(@PathVariable Integer id) {
+        Job job = service.getDetail(1);
+        return new Message(0, job);
+    }
+
+    @RequestMapping("/add")
+    public Message add(Job job) {
+        if (service.add(job)) {
+            return new Message(0, "添加信息成功");
+        } else {
+            return new Message(500, "添加信息失败");
+        }
+    }
+
+    @RequestMapping("/delete/{id}")
+    public Message delete(@PathVariable Integer id) {
+        if (service.delete(id)) {
+            return new Message(0, "删除信息成功");
+        } else {
+            return new Message(500, "删除信息失败");
+        }
+    }
+
+    @RequestMapping("/sendEmail")
+    public Message sendEmail(User user) {
+        service.sendEmail(user);
+        return new Message(0, null);
+    }
+}
---
+++ src/com/horvey/pojo/User.java
@@ -0,0 +1,77 @@
+package com.horvey.pojo;
+
+/**
+ * 用户实体类
+ */
+public class User {
+
+    private int id;
+
+    private String name;    //姓名
+
+    private String classes; //班级
+
+    private String phone;   //手机
+
+    private String email;   //邮箱
+
+    public User() {
+    }
+
+    public User(String name, String classes, String phone, String email) {
+        this.name = name;
+        this.classes = classes;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClasses() {
+        return classes;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", classes='" + classes + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
